f1f40f13eea3f4969b7c637480b5d99920262310
Karthik Devaraj <karthikdevaraj1017@gmail.com>
2019-02-14 12:47:53 +0530

    week5 java laab

---
+++ java_lab/week5/flames.java
@@ -0,0 +1,32 @@
+import java.io. *;
+import java.util. *;
+public class Flames{
+    String str1 = "karthik";
+    String str2 = "devaraj";
+    String agent = "flames";
+    int cutlen = 0,total = 0,len = 6,alen = 0;
+    public static cancel(){
+        for(int i = 0; i < str1.length(); i++){
+            for(int j = 0; j < str2.length(); j++){
+                if(str1[i] == str2[j]){
+                    str[j] = '\n';
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < str1.length(); i++){
+            if(str1[i] != '\n'){
+                total++;
+            }
+        }
+        for(int i = 0; i < str2.length(); i++){
+            if(str2[i] != '\n'){
+                total++;
+            }
+        }
+        while(len != 1){
+
+        }
+    }
+
+}
