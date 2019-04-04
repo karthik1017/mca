f3d7b8a1a7aba6b733f45c7e75be1e57b82807aa
karthik1017 <karthikdevaraj1017@gmail.com>
2019-04-04 15:40:24 +0530

    bank.java changed

--- java_lab/week6/Bank.java
+++ java_lab/week6/Bank.java
@@ -1,5 +1,7 @@
 	import java.io. *;
 	import java.util. *;
+	import java.util.Scanner; 
+
 	class Bank{
 		void displayBank(){
 			System.out.println("welcome to KARTHIK BANKERS CO.");
@@ -39,19 +41,30 @@
 
 	class stat{
 		public static void main(String[] args) {
-			Vloan v = new Vloan();
-			v.displayBank();
-			v.displayLoan();
-			v.displayVloan();
-
-			HomeLoan h = new HomeLoan();
-			h.displayBank();
-			h.displayLoan();
-			h.displayHloan();
-
-			PersonalLoan P = new PersonalLoan();
-			P.displayBank();
-			P.displayLoan();
-			P.displayPloan();
+            Scanner sc = new Scanner(System.in);
+			System.out.printf("\n 1)vehicle loan \n 2)personal loan \n 3)homeloan")
+			System.out.println("enter ur choice");
+			int choice = sc.nextInt();
+			switch(choice) {
+            case 1:
+			       Vloan v = new Vloan();
+			       v.displayBank();
+			       v.displayLoan();
+			       v.displayVloan();
+                   break;
+            case 2:
+			       PersonalLoan P = new PersonalLoan();
+			       P.displayBank();
+			       P.displayLoan();
+			       P.displayPloan();
+                   break;
+			case 3:
+			       HomeLoan h = new HomeLoan();
+			       h.displayBank();
+			       h.displayLoan();
+			       h.displayHloan();
+			       break;
+            default:
+}                  System.out.println('wrong choice')
 		}
 	}
