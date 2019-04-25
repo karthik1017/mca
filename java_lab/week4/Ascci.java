12ead9899182a81f5fd3ee14c7ddb5b40f3643ae
karthik1017 <karthikdevaraj1017@gmail.com>
2019-04-25 11:46:19 +0530

    small change-2

--- java_lab/week4/Ascci.java
+++ java_lab/week4/Ascci.java
@@ -1,10 +1,5 @@
 import java.io. *;
 
 class Asccii{
-    String str= "i am karthik MCA student";
-    String[] str1 = str.split(" ");
-    for (int i=0; i < arrSplit.length; i++)
-    {
-      for(int j=0; j < )
-    }
+
 }
